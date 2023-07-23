e832edf881b03a0e41d2bb8e776622745bb9db0b
danielkarcz14 <125573628+danielkarcz14@users.noreply.github.com>
2023-07-23 21:09:44 +0200

    Add files via upload

---
+++ caesar.c
@@ -0,0 +1,79 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include <stdlib.h>
+
+// First project where I had to dig for some more information on the internet to figure out only this formula c = (c - 97 + key) % 26 + 97; :( 
+
+bool only_digits(string s);
+char rotate(int key, char c);
+
+int main(int argc, string argv[])
+{
+    // Only two arguments allowed
+    if (argc != 2)
+    {
+        printf("Usage: ./caesar key\n");
+        return 1;
+    }
+    string s = argv[1];
+
+    // Argument has to be digit
+    if (only_digits(s) == false)
+    {
+        printf("Usage: ./caesar key\n");
+        return 1;
+    }
+    // Convert argv[1] to an integer
+    int key = atoi(argv[1]);
+
+    // Input from user
+    string plaintext = get_string("plaintext:  ");
+    printf("ciphertext: ");
+    // Count string lenght
+    int len2 = strlen(plaintext);
+    // Iterate through each array in string
+    for (int i = 0; i < len2; i++)
+    {
+        // Call rotate function of each arrau in plaintext
+        char c = rotate(key, plaintext[i]);
+        printf("%c", c);
+    }
+    printf("\n");
+}
+
+
+bool only_digits(string s)
+{
+    int len = strlen(s);
+    for (int i = 0; i < len; i++)
+    {
+        if (!isdigit(s[i]))
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+char rotate(int key, char c)
+{
+    // Check if array in string plaintext is alphabetical and lowercase
+    if (isalpha(c) && islower(c))
+    {
+        c = (c - 97 + key) % 26 + 97;
+
+    }
+    // Check if array in string plaintext is alphabetical and uppercase
+    else if (isalpha(c) && isupper(c))
+    {
+        c = (c - 65 + key) % 26 + 65;
+    }
+    // If conditions are not met function returns what user typed
+    else
+    {
+        return c;
+    }
+    return c;
+}
