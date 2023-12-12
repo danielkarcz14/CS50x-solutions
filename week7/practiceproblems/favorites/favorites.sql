b3f71b5c653374e983098d56edb49792210a5cfd
danielkarcz14 <125573628+danielkarcz14@users.noreply.github.com>
2023-12-12 22:18:14 +0100

    Create favorites.sql

---
+++ week7/practiceproblems/favorites/favorites.sql
@@ -0,0 +1,21 @@
+UPDATE shows SET title = "Adventure Time" WHERE title = "adventure time";
+UPDATE shows SET title = "Arrow" WHERE title = "arrow";
+UPDATE shows SET title = "Avatar: The Last Airbender" WHERE title = "Avatar the Last Airbender";
+UPDATE shows SET title = "Brooklyn Nine-Nine" WHERE title = "B99";
+UPDATE shows SET title = "Community" WHERE title = "community";
+UPDATE shows SET title = "Family Guy" WHERE title = "family guy";
+UPDATE shows SET title = "Friends" WHERE title = "friends";
+UPDATE shows SET title = "Game of Thrones" WHERE title = "Game of Thrones";
+UPDATE shows SET title = "Gilmore Girls" WHERE title = "Gilmore girls";
+UPDATE shows SET title = "Grey's Anatomy" WHERE title = "Grey's anatomy";
+UPDATE shows SET title = "It's Always Sunny in Philadelphia" WHERE title = "Its Always Sunny in Philidelphia";
+UPDATE shows SET title = "Parks and Recreation" WHERE title = "Parks and Rec";
+UPDATE shows SET title = "Sherlock" WHERE title = "sherlock holmes";
+UPDATE shows SET title = "Squid Game" WHERE title = "squid game";
+UPDATE shows SET title = "The Bachelorette" WHERE title = "the bachelorette";
+UPDATE shows SET title = "The Crown" WHERE title = "The CROWN";
+UPDATE shows SET title = "The Office" WHERE LIKE = "%office%" or "%Office%";
+UPDATE shows SET title = "The Queen's Gambit" WHERE title = "Queen's Gambit";
+UPDATE shows SET title = "The Untamed" WHERE title = "the Untamed";
+
+
