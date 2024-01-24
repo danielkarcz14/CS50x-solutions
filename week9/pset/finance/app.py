661630152e13bcb866e1554a479a9d529f22f727
danielkarcz14 <125573628+danielkarcz14@users.noreply.github.com>
2024-01-24 19:18:23 +0100

    Update app.py

--- week9/pset/finance/app.py
+++ week9/pset/finance/app.py
@@ -1,7 +1,3 @@
-# Not finished yet two cs50 checks missing
-# I will also publish version with sqlalchemy instead of cs50sql library
-
-
 import os
 
 from cs50 import SQL
@@ -42,10 +38,14 @@ def after_request(response):
 @login_required
 def index():
     """Show portfolio of stocks"""
-    # get attributes
-    shares = db.execute("SELECT symbol, SUM(quantity), share_price, SUM(total_price) FROM logs WHERE user_id = ? GROUP BY symbol", session["user_id"])
+    # get attributes\
+    shares = db.execute("SELECT symbol, quantity, share_price, total_price FROM portfolio WHERE user_id = ?", session["user_id"])
     balance = db.execute("SELECT cash FROM users WHERE id = ?", session["user_id"])[0]['cash']
-    return render_template("index.html", shares=shares, balance=round(balance, 2))
+    total_stock = db.execute("SELECT SUM(total_price) FROM portfolio WHERE user_id = ?", session["user_id"])[0]["SUM(total_price)"]
+    if total_stock is None:
+        total_stock = 0
+    total_value = balance + total_stock
+    return render_template("index.html", shares=shares, balance=round(balance, 2), total_value=total_value)
 
 
 @app.route("/buy", methods=["GET", "POST"])
@@ -53,7 +53,7 @@ def index():
 def buy():
     """Buy shares of stock"""
     if request.method == "POST":
-        symbol = request.form.get("symbol")
+        symbol = request.form.get("symbol").upper()
         quantity = request.form.get("shares")
         try:
             quantity = int(quantity)
@@ -78,12 +78,29 @@ def buy():
         # update db
         db.execute("UPDATE users SET cash = ? WHERE id = ?", new_cash, session["user_id"])
 
-        # add record to logs
+        # add record to transactions
         time = datetime.now()
-        db.execute("INSERT INTO logs (symbol, quantity, share_price, total_price, time, user_id) VALUES(?, ?, ?, ?, ?, ?)",
-                   symbol.upper(), quantity, stock_price, total_price, time, session["user_id"])
+        db.execute("INSERT INTO transactions (symbol, quantity, share_price, time, user_id) VALUES(?, ?, ?, ?, ?)",
+                   symbol.upper(), quantity, stock_price, time, session["user_id"])
 
-        return redirect("/")
+        # add new record to portfolio if one does not exists for the symbol
+        if not db.execute("SELECT symbol FROM portfolio WHERE symbol = ? AND user_id = ?", symbol, session["user_id"]):
+            db.execute("INSERT INTO portfolio (symbol, quantity, share_price, total_price, time, user_id) VALUES(?, ?, ?, ?, ?, ?)",
+                       symbol.upper(), quantity, stock_price, total_price, time, session["user_id"])
+            return redirect("/")
+
+        else:
+            # or update existing record
+            old_quantity = db.execute("SELECT quantity FROM portfolio WHERE user_id = ? AND symbol = ?",
+                                      session["user_id"], symbol)[0]["quantity"]
+            new_quantity = old_quantity + quantity
+
+            old_total_price = db.execute("SELECT total_price FROM portfolio WHERE user_id = ? AND symbol = ?",
+                                         session["user_id"], symbol)[0]["total_price"]
+            new_total_price = old_total_price + total_price
+            db.execute("UPDATE portfolio SET quantity = ?, share_price = ?, total_price = ? WHERE symbol = ? AND user_id = ?",
+                       new_quantity, stock_price, new_total_price, symbol, session["user_id"])
+            return redirect("/")
 
     return render_template("buy.html")
 
@@ -92,7 +109,8 @@ def buy():
 @login_required
 def history():
     """Show history of transactions"""
-    return apology("TODO")
+    transactions = db.execute("SELECT symbol, quantity, share_price, time FROM transactions WHERE user_id = ?", session["user_id"])
+    return render_template("history.html", transactions=transactions)
 
 
 @app.route("/login", methods=["GET", "POST"])
@@ -151,7 +169,7 @@ def quote():
     """Get stock quote."""
     if request.method == "POST":
         symbol = request.form.get("symbol")
-        #print(f"return of lookup = {lookup(symbol)}")
+        # print(f"return of lookup = {lookup(symbol)}")
         if lookup(symbol) is None:
             return apology("INVALID SYMBOL", 400)
         return render_template("quoted.html", symbol=symbol, price=lookup(symbol)['price'])
@@ -174,6 +192,10 @@ def register():
         if db.execute("SELECT username FROM users WHERE username = ?", name):
             return apology("Username already exists", 400)
 
+        result = password_check(password)
+        if result is not None:
+            return result
+
         hashed_password = generate_password_hash(password)
         # print(f"{name} {hashed_password}")
         db.execute("INSERT INTO users (username, hash) VALUES(?, ?)", name, hashed_password)
@@ -186,9 +208,9 @@ def register():
 @login_required
 def sell():
     """Sell shares of stock"""
-    user_symbols = db.execute("SELECT DISTINCT symbol FROM logs WHERE user_id = ?", session["user_id"])
+    user_symbols = db.execute("SELECT DISTINCT symbol FROM portfolio WHERE user_id = ?", session["user_id"])
     if request.method == "POST":
-        symbol = request.form.get("symbol")
+        symbol = request.form.get("symbol").upper()
         if not symbol:
             return apology("Select symbol please", 400)
 
@@ -202,17 +224,16 @@ def sell():
 
         # check if user has enough shares to sell
         current_quantity = db.execute("""
-                                   SELECT SUM(quantity) FROM logs
+                                   SELECT SUM(quantity) FROM portfolio
                                    WHERE user_id = ? AND symbol = ?
                                    GROUP BY SYMBOL
                                    """,
-                                   session["user_id"], symbol
-                                   )[0]['SUM(quantity)']
+                                      session["user_id"], symbol
+                                      )[0]['SUM(quantity)']
         current_quantity = int(current_quantity)
 
         if quantity > current_quantity:
             return apology("You don't have that many shares", 400)
-
 
         # update cash
         stock_price = lookup(symbol)['price']
@@ -222,18 +243,38 @@ def sell():
 
         db.execute("UPDATE users SET cash = ? WHERE id = ?", new_balance, session["user_id"])
 
-        # update shares amount in table logs
+        # update existing record
         new_quantity = current_quantity - quantity
-        db.execute("UPDATE logs SET quantity = ? WHERE user_id = ? AND symbol = ?", new_quantity, session["user_id"], symbol)
+        old_total_price = db.execute("SELECT total_price FROM portfolio WHERE user_id = ? AND symbol = ?",
+                                     session["user_id"], symbol)[0]["total_price"]
+        new_total_price = old_total_price - total_price
+        db.execute("UPDATE portfolio SET quantity = ?, share_price = ?, total_price = ? WHERE symbol = ? AND user_id = ?",
+                   new_quantity, stock_price, new_total_price, symbol, session["user_id"])
 
-        # update shares amount in table logs
-        new_total_price = new_quantity * stock_price
-        db.execute("UPDATE logs SET total_price = ? WHERE user_id = ? AND symbol = ?", new_total_price, session["user_id"], symbol)
-
-        # delete from logs if quantity is = 0
+        # delete from portfolio if quantity is = 0
         if new_quantity == 0:
-            db.execute("DELETE FROM logs WHERE user_id = ? AND symbol = ?", session["user_id"], symbol)
+            db.execute("DELETE FROM portfolio WHERE user_id = ? AND symbol = ?", session["user_id"], symbol)
+
+        # add record to transactions
+        quantity = quantity * -1
+        time = datetime.now()
+        db.execute("INSERT INTO transactions (symbol, quantity, share_price, time, user_id) VALUES(?, ?, ?, ?, ?)",
+                   symbol.upper(), quantity, stock_price, time, session["user_id"])
 
         return redirect("/")
 
     return render_template("sell.html", user_symbols=user_symbols)
+
+
+def password_check(string):
+    if len(string) < 6:
+        return apology("At least 6 characters", 400)
+
+    if not any(char.isdigit() for char in string):
+        return apology("Must contain atleast one digit", 400)
+
+    if not any(char.isupper() for char in string):
+        return apology("Must contain atleast one uppercase letter", 400)
+
+    if not any(char.islower() for char in string):
+        return apology("Must contain atleast one lowercase letter", 400)
